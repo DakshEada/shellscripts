3bd36db6f7fc3a8e50912dafabc5a808cabd19eb
deadak <abhinand.eada@gmail.com>
2018-10-31 11:56:47 -0700

    checks file conditions

--- fileconditions.sh
+++ fileconditions.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [ -f "$1" ]
+if [ -f $1 ]
 then
 	echo "$1 is a file"
 else
 	echo "$1 is not a file"
+fi
+
+echo "exist status is $?"
+
+if [ $? = 0 ]; then
+echo "process executed successfully"
+else
+echo "process did not execute"
 fi
