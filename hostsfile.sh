15f7c64b83414a156eeec0b938ce00a559e66153
deadak <abhinand.eada@gmail.com>
2018-10-31 11:57:07 -0700

    check hosts

---
+++ hostsfile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#contains server ip address
+
+serverlist=(52.14.105.103 18.220.166.106 52.14.190.214 18.218.153.51)
+
+
+#want to print out the value at a position
+
+echo ${serverlist[2]}
